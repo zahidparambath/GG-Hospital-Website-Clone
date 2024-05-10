4845324dad5750e31f07b8db20c77479eb121082
Zahid Parambath <pzahid10102001@gmail.com>
2024-05-11 01:09:30 +0530

    feat: added sections and footer

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const carousel = document.getElementById('carouselSlides');
+        const carouselInstance = new bootstrap.Carousel(carousel);
+        setTimeout(function() {
+            carouselInstance.cycle();
+        }, 4000); 
+
+const carousel2 = document.getElementById('carouselSlide2');
+const carouselInstance2 = new bootstrap.Carousel(carousel);
+setTimeout(function() {
+            carouselInstance.cycle();
+        }, 4000);
+        
+const carousel3 = document.getElementById('carouselSlide3');
+const carouselInstance3 = new bootstrap.Carousel(carousel);
+setTimeout(function() {
+carouselInstance.cycle();
+}, 4000);
